cfb2cfddc7d7e037ec50abe1a21691eb18a32190
Adam Baldoni <abaldoni19@gmail.com>
2021-06-08 14:23:34 -0400

    Add existing file

--- GNN.py
+++ GNN.py
@@ -9,8 +9,9 @@ except ImportError:
 	import keras
 
 from caloGraphNN_keras import *
+from spektral.layers.convolutional import edge_conv
 
-def open_file():
+def open_file(name=None):
 	'''
 	Opens all 16 npz files and concatenates them
 	'''
@@ -20,7 +21,7 @@ def open_file():
 	pad_events = []
 	pad_labels = []
 	for i in range(1, 17):
-		data = np.load(f'{i}.npz', allow_pickle=True)
+		data = np.load(f'{name}_{i}.npz', allow_pickle=True)
 		events.append(data['events'])
 		labels.append(data['labels'])
 		norm_events.append(data['norm_events'])
@@ -57,3 +58,67 @@ class GravNetModel(keras.Model):
 		layer = cls(*args, **kwargs)
 		self._layers.append(layer)
 		return layer
+
+class EdgeConvModel(keras.Model):
+	'''
+	A model using Spektral's EdgeConv layer
+	'''
+	def __init__(self, channels=32):
+
+		super(EdgeConvModel, self).__init__()
+		momentum = 0.99
+
+		self.edgeconv = self.add_layer(edge_conv.EdgeConv, channels, name='edge_conv')
+		self.batchnorm = self.add_layer(keras.layers.BatchNormalization, momentum=momentum, name='batchnorm')
+		self.output_dense_1 = self.add_layer(keras.layers.Dense, 16, activation='relu', name='output_1')
+		self.output_dense_2 = self.add_layer(keras.layers.Dense, 1, activation='sigmoid', name='output_2')
+
+	def call(self, inputs):
+		print(inputs)
+		if len(inputs) == 2:
+#			x, a = inputs
+			x = inputs[0]
+			a = inputs[1]
+		else:
+#			x, a, _ = inputs
+			x = inputs[1]
+			a = inputs[2]
+#		x, a = inputs
+		print(x)
+		print(a)
+		x = self.edgeconv([x, a])
+		x = self.batchnorm(x)
+		x = self.output_dense_1(x)
+		output = self.output_dense_2(x)
+		return output
+
+	def add_layer(self, cls, *args, **kwargs):
+		layer = cls(*args, **kwargs)
+		self._layers.append(layer)
+		return layer
+
+
+class GarNetModel(keras.Model):
+    '''
+	Keras Model with GarNet layer
+	'''
+    def __init__(self, aggregators=4, filters=20, propagate=22, input_format='x'):
+        super(GarNetModel, self).__init__()
+        momentum = 0.99
+
+        self.garnet = self.add_layer(GarNet, aggregators, filters, propagate, input_format='x', mean_by_nvert=True, name='garnet')
+        self.batchnorm = self.add_layer(keras.layers.BatchNormalization, momentum=momentum, name='batchnorm')
+        self.output_dense_1 = self.add_layer(keras.layers.Dense, 32, activation='relu', name='output_1')
+        self.output_dense_2 = self.add_layer(keras.layers.Dense, 1, activation='sigmoid', name='output_2')
+        
+    def call(self, inputs):
+        x = self.garnet(inputs)
+        x = self.batchnorm(x)
+        x = self.output_dense_1(x)
+        output = self.output_dense_2(x)
+        return output
+    
+    def add_layer(self, cls, *args, **kwargs):
+        layer = cls(*args, **kwargs)
+        self._layers.append(layer)
+        return layer
